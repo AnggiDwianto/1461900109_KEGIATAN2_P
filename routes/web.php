7fc363ca0e50462abf27660fbfe0a4e6b951124f
AnggiDwianto <anggidwianto654@gmail.com>
2021-05-29 14:27:06 +0700

    aktifitas2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PerpusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('mahasiswa_0109', PerpusController::class);
